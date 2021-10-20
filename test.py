b0c4fffb8d91694f60783827f12977e48afbdf6e
Nitin Khanna <mail@nitinkhanna.com>
2021-10-20 13:49:00 -0700

    testing out a new way to call tts

--- test.py
+++ test.py
@@ -9,8 +9,23 @@
 from TTS.utils.synthesizer import Synthesizer
 from TTS.utils.manage import ModelManager
 
+model_path = None
+config_path = None
+speakers_file_path = None
+vocoder_path = None
+vocoder_config_path = None
+encoder_path = None
+encoder_config_path = None
+use_cuda = False
+
 model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
 vocoder_name = 'vocoder_models/en/ljspeech/hifigan_v2'
+
+path = Path(__file__).parent / "../.models.json"
+manager = ModelManager(path)
+
+model_path, config_path, model_item = manager.download_model(model_name)
+vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
 
 synthesizer = Synthesizer(
     model_path,
@@ -20,5 +35,8 @@ synthesizer = Synthesizer(
     vocoder_config_path,
     encoder_path,
     encoder_config_path,
-    args.use_cuda,
+    use_cuda,
 )
+
+wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
+synthesizer.save_wav(wav, './ttsoutput/test_output.wav')
