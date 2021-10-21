f662f6b27fa809382ddf882dbcad5c1bb8bfb134
Nitin Khanna <mail@nitinkhanna.com>
2021-10-20 17:09:45 -0700

    spaces not tabs

--- test.py
+++ test.py
@@ -16,36 +16,36 @@ workspace_path = os.environ['GITHUB_WORKSPACE']
 
 
 def thisMakesAudio(
-	speech_text,
-	output_file_path,
-	model_name = 'tts_models/en/ljspeech/tacotron2-DDC',
-	vocoder_name = 'vocoder_models/en/ljspeech/hifigan_v2'):
-	path = Path(__file__).parent / "../.models.json"
-	manager = ModelManager(path)
+    speech_text,
+    output_file_path,
+    model_name = 'tts_models/en/ljspeech/tacotron2-DDC',
+    vocoder_name = 'vocoder_models/en/ljspeech/hifigan_v2'):
+    path = Path(__file__).parent / "../.models.json"
+    manager = ModelManager(path)
 
-	model_path, config_path, model_item = manager.download_model(model_name)
-	vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
+    model_path, config_path, model_item = manager.download_model(model_name)
+    vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
 
-	synthesizer = Synthesizer(
-	    model_path,
-	    config_path,
-	    speakers_file_path,
-	    vocoder_path,
-	    vocoder_config_path,
-	    encoder_path,
-	    encoder_config_path,
-	    use_cuda,
-	)
+    synthesizer = Synthesizer(
+        model_path,
+        config_path,
+        speakers_file_path,
+        vocoder_path,
+        vocoder_config_path,
+        encoder_path,
+        encoder_config_path,
+        use_cuda,
+    )
 
-	wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
-	workspace_path = os.environ['GITHUB_WORKSPACE']
-	synthesizer.save_wav(wav, workspace_path + '/ttsoutput/test_output.wav')
+    wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
+    workspace_path = os.environ['GITHUB_WORKSPACE']
+    synthesizer.save_wav(wav, workspace_path + '/ttsoutput/test_output.wav')
 
 if __name__ == '__main__':
-	f = open('./data.json')
-	data = json.load(f)
-	for item in data:
-		txt = item['summarized_article'][0]['summary_text']
-    	uuid = item['uuid']
-    	file_path = workspace_path + '/ttsoutput/' + uuid + '.wav'
+    f = open('./data.json')
+    data = json.load(f)
+    for item in data:
+        txt = item['summarized_article'][0]['summary_text']
+        uuid = item['uuid']
+        file_path = workspace_path + '/ttsoutput/' + uuid + '.wav'
     thisMakesAudio(txt, file_path)
