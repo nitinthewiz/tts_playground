43946bbc105041ada55f86f2a1d7750153f60b6b
Nitin Khanna <mail@nitinkhanna.com>
2021-10-20 17:06:15 -0700

    uuids and data file and  thisMakesAudio function

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-# import torch
-# synthesizer = torch.hub.load('coqui-ai/TTS:v0.2.0', 
-#                              'tts', 
-#                              source='github')
-# wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
-# synthesizer.save_wav(wav, './ttsoutput/test_output.wav')
-
 import os
 from pathlib import Path
 from TTS.utils.synthesizer import Synthesizer
@@ -19,26 +12,40 @@ encoder_path = None
 encoder_config_path = None
 use_cuda = False
 
-model_name = 'tts_models/en/ljspeech/tacotron2-DDC'
-vocoder_name = 'vocoder_models/en/ljspeech/hifigan_v2'
-
-path = Path(__file__).parent / "../.models.json"
-manager = ModelManager(path)
-
-model_path, config_path, model_item = manager.download_model(model_name)
-vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
-
-synthesizer = Synthesizer(
-    model_path,
-    config_path,
-    speakers_file_path,
-    vocoder_path,
-    vocoder_config_path,
-    encoder_path,
-    encoder_config_path,
-    use_cuda,
-)
-
-wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
 workspace_path = os.environ['GITHUB_WORKSPACE']
-synthesizer.save_wav(wav, workspace_path + '/ttsoutput/test_output.wav')
+
+
+def thisMakesAudio(
+	speech_text,
+	output_file_path,
+	model_name = 'tts_models/en/ljspeech/tacotron2-DDC',
+	vocoder_name = 'vocoder_models/en/ljspeech/hifigan_v2'):
+	path = Path(__file__).parent / "../.models.json"
+	manager = ModelManager(path)
+
+	model_path, config_path, model_item = manager.download_model(model_name)
+	vocoder_path, vocoder_config_path, _ = manager.download_model(vocoder_name)
+
+	synthesizer = Synthesizer(
+	    model_path,
+	    config_path,
+	    speakers_file_path,
+	    vocoder_path,
+	    vocoder_config_path,
+	    encoder_path,
+	    encoder_config_path,
+	    use_cuda,
+	)
+
+	wav = synthesizer.tts("This is an open-source library that generates synthetic speech!")
+	workspace_path = os.environ['GITHUB_WORKSPACE']
+	synthesizer.save_wav(wav, workspace_path + '/ttsoutput/test_output.wav')
+
+if __name__ == '__main__':
+	f = open('./data.json')
+	data = json.load(f)
+	for item in data:
+		txt = item['summarized_article'][0]['summary_text']
+    	uuid = item['uuid']
+    	file_path = workspace_path + '/ttsoutput/' + uuid + '.wav'
+    thisMakesAudio(txt, file_path)
